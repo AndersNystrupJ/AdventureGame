4b3451d792a47615230b5e2a329414b66ab0d015
AndersNystrupJ <anje0008@stud.kea.dk>
2024-03-04 10:10:10 +0100

    Adventure Game projekt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("Hello Earth!");
         System.out.println("Hi");
+        System.out.println(2+2);
     }
 }
