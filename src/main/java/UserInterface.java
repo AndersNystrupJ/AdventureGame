c52ba4e7e9b432353945846ab581bd3db7c89531
git-armu <armu0001@stud.kea.dk>
2024-03-05 09:58:47 +0100

    Menu færdig, mangler metoderne og room

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -3,9 +3,44 @@ import org.example.Room;
 import java.util.Scanner;
 public class UserInterface {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Adventure AdventureGame = new Adventure();
+        Adventure adventure = new Adventure();
 
+        public void startGame() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Velkommen til Adventure Game");
+            System.out.println("Skriv 'hjælp' for intruktioner til spillet");
+            System.out.println();
+
+            while (true) { //infinite loop til break; bruges
+                String menuOption = scanner.nextLine();
+
+                if (menuOption.equals("n")) {
+                    adventure.goNorth();
+                } else if (menuOption.equals("w")) {
+                    adventure.goWest();
+                } else if (menuOption.equals("e")) {
+                    adventure.goEast();
+                } else if (menuOption.equals("s")) {
+                    adventure.goSouth();
+                } else if (menuOption.equals("hjælp")) {
+                    System.out.println("Kommandoer:");
+                    System.out.println(" 'n' for at gå mod nord");
+                    System.out.println(" 'w' for at gå mod vest");
+                    System.out.println(" 'e' for at gå mod øst");
+                    System.out.println(" 's' for at gå mod syd");
+                    System.out.println(" 'exit' for at gå ud af spillet");
+                    System.out.println(" 'look' for at kigge rundt i dit rum");
+                } else if (menuOption.equals("exit")) {
+                    System.out.println("Du har afsluttet spillet");
+                    break;
+                } else {
+                    System.out.println("Ugyldigt. Skriv 'hjælp' for instruktioner til spillet.");
+                }
+            }
+        }
+
+
+        /*
         System.out.println(AdventureGame.getCurrentRoom().getRoomNumber());
         System.out.println(AdventureGame.getCurrentRoom().getRoomDescription());
         System.out.println("1. Go North");
@@ -32,9 +67,9 @@ public class UserInterface {
                 System.out.println("You have chosen to exit.");
                 break;
             }
-
         }
 
-    }
+         */
 
+    }
 }
--- src/main/java/org/example/Adventure.java
+++ src/main/java/org/example/Adventure.java
@@ -5,11 +5,18 @@ public class Adventure {
     public Adventure() {
         buildAdventure();
     }
+
     public void buildAdventure() {
         Room room1 = new Room("Room 1", "An empty room with with two doors. There's an unpleasent smell lingering in the air...");
+        Room room2 = new Room("Room 2", "A bloody room with guts on the floor...");
+        Room room3 = new Room("Room 3", "A room with weird symbols on the wall");
+        Room room4 = new Room("Room 4", "A room with Fie Laursen");
+        Room room5 = new Room("Room 5", "lalala");
+        Room room6 = new Room("Room 6", "hej");
+        Room room7 = new Room("Room 7", "med");
+        Room room8 = new Room("Room 8", "dig");
 
         currentRoom = room1;
-
     }
 
     public Room getCurrentRoom() {
@@ -18,5 +25,18 @@ public class Adventure {
 
     public void look() {
         System.out.println(currentRoom.getRoomDescription());
+    }
+
+    // vores metoder til go north, east, osv
+    public void goNorth() {
+    }
+
+    public void goWest() {
+    }
+
+    public void goEast() {
+    }
+
+    public void goSouth() {
     }
 }
