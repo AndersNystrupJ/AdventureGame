f73884ea780c67d7861a902ac05359eb3029fe2d
GitOnat <maon0001@stud.kea.dk>
2024-03-08 12:00:58 +0100

    takeItem in player method

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -6,22 +6,10 @@ public class Player {
     private Room inRoom;
     private ArrayList<Item> items;
 
-    public Player(String playerName, Room inRoom, ArrayList<Item> items){
+    public Player(String playerName, Room inRoom, ArrayList<Item> inventory){
         this.playerName = playerName;
         this.inRoom = inRoom;
         this.items = items;
-    }
-
-    public String getPlayerName() {
-        return playerName;
-    }
-
-    public Room getInRoom() {
-        return inRoom;
-    }
-
-    public ArrayList<Item> getItems() {
-        return items;
     }
 
     public void takeItem(String itemToTake) {
