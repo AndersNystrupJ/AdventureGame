14776e3a4de016972882e98116d8c25ffa9898b2
AnderNystrupJ <anje0008@stud.kea.dk>
2024-03-20 12:23:51 +0100

    Changes by kasperhejgaard

--- src/main/java/org/example/Enemy.java
+++ src/main/java/org/example/Enemy.java
@@ -1,4 +1,35 @@
 package org.example;
 
 public class Enemy {
+    private String name;
+    private String description;
+    private int health;
+    private Weapon enemyWeapon;
+
+    public Enemy(String name, String description, int health, Weapon enemyWeapon) {
+        this.name = name;
+        this.description = description;
+        this.health = health;
+        this.enemyWeapon = enemyWeapon;
+    }
+
+    public void enemyAttack() {
+            enemyWeapon.uses();
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public Weapon getEnemyWeapon() {
+        return enemyWeapon;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
